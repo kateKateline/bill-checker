a67b235afe3553877f8830307548f1ee20c6d109
KateKateline <rizkyramadhan12098@gmail.com>
2026-01-02 00:14:07 +0700

    icon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BillCheck - Analisis Transparansi Tagihan Rumah Sakit</title>
-    
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Source+Sans+3:wght@400;600;700&display=swap" rel="stylesheet">
-    
+
     @vite('resources/css/app.css')
 </head>
+
 
 <body class="bg-slate-50 text-slate-900 min-h-screen flex flex-col font-['Inter']">
 
